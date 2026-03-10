0ada31f4c08fede99c75e685311d8030f8665c9f
t0mbo192 <tomlett192@gmail.com>
2026-03-10 05:53:57 -0400

    inline markdown is functional

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,7 @@
 import unittest
 from textnode import TextType, TextNode
 from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
-from inline_markdown import text_to_textnodes
+from inline_markdown import text_to_textnodes, markdown_to_blocks
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -128,6 +128,28 @@ class TestInlineMarkdown(unittest.TestCase):
         ]
         nodes = text_to_textnodes(text)
         self.assertEqual(nodes, expected_nodes)
+
+    # markdown to blocks tests
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
 
 
 if __name__ == "__main__":
